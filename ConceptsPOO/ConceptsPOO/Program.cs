648846b8d72e13ea3d34f2cc96746196b682be34
alexanderrincon9 <elkbe99@gmail.com>
2022-03-31 20:44:54 -0500

    finish Date class

---
+++ ConceptsPOO/ConceptsPOO/Program.cs
@@ -0,0 +1,21 @@
+﻿
+using ConceptsPOO;
+
+Console.WriteLine("POO Concepts");
+Console.WriteLine("==================");
+
+
+try
+{
+    Console.WriteLine(new Date(2024, 2, 29));
+
+    Console.WriteLine(new Date(2022, 11, 31));
+
+    Console.WriteLine(new Date(2022, 3, 31));
+
+}
+catch (Exception error)
+{
+
+    Console.WriteLine(error.Message);
+}
